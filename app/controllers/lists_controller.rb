e83cf030b65d6b27f040baf3952b5c499f07bb63
EC2 Default User <ec2-user@ip-172-31-29-186.ap-northeast-1.compute.internal>
2023-03-12 00:55:41 +0000

    a

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,24 +4,41 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+    redirect_to list_path(@list.id)
+  else
+    render :new
   end
+end
 
   def show
     @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
   end
   
   def index
    @lists = List.all
   end
   
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to
+  end
+  
   private
+  
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
